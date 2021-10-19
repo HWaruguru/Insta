0da156660e46a41b2ebb3c410615db890612a22f
HWaruguru <hannah.waruguru8@gmasil.com>
2021-10-19 13:36:41 +0300

    Add a logout and login url

--- insta/urls.py
+++ insta/urls.py
@@ -1,13 +1,17 @@
 
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns=[
     url(r'^admin/', admin.site.urls),
     url('^$', views.index, name = 'index'),
-]
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url( r'^login/$',auth_views.LoginView.as_view(template_name="useraccounts/login.html"), name="login"),
+    url(r'^logout/$', auth_views.logout, {"next_page": '/'}), 
+] 
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
