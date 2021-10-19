d8a24162f261c236569a012a9a06e74f2129e358
HWaruguru <hannah.waruguru8@gmasil.com>
2021-10-19 13:37:08 +0300

    Add a login required

--- insta/views.py
+++ insta/views.py
@@ -1,7 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from .models import Post
 
-
+@login_required(login_url='/accounts/login/')
 def index(request):
     follows = ['peterthitu', 'gathu', 'kieya', 'alice', 'mary']
     posts = Post.objects.select_related().all()
